ac6047fd303f876db6f30671b221909bd954dbf8
Laraib Khan <m.laraibkhan@outlook.com>
2025-10-29 13:35:26 +0100

    Revert "Codespace unearthly monster 5vx4xpr7qgphvxwv"

--- backend/src/models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Database models."""
-from src.models.target import Target
-from src.models.scan_session import ScanSession
-from src.models.finding import Finding
-from src.models.ai_analysis import AIAnalysis
-
-__all__ = ["Target", "ScanSession", "Finding", "AIAnalysis"]
